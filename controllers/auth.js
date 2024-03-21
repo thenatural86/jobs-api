5b07c89493ce30e40e89d667e0d9ea8c37373f29
thenatural86 <philip.smalls86@gmail.com>
2024-03-21 11:09:47 -0300

    refactor

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
