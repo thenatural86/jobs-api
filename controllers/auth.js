8159d4f256695810a9c7da4425b15e5acf1844de
thenatural86 <philip.smalls86@gmail.com>
2024-03-21 11:09:22 -0300

    error checking controller example

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,10 @@
-const register = (req, res) => {
-  res.send('register user')
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body })
+  res.status(StatusCodes.CREATED).json({ user })
 }
 const login = (req, res) => {
   res.send('login user')
