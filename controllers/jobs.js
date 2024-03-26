72f2558aea2156b0eeb0744e79bd0d7d09f2fd75
thenatural86 <philip.smalls86@gmail.com>
2024-03-26 10:42:24 -0300

    create job route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,17 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
 const getAllJobs = (req, res) => {
   res.send('get all jobs')
 }
 const getJob = (req, res) => {
   res.send('get job')
 }
-const createJob = (req, res) => {
-  res.json(req.user)
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
 const updateJob = (req, res) => {
   res.send('update job')
