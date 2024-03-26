37d78a74ba614411b6591377669d7d4fc3659dcb
thenatural86 <philip.smalls86@gmail.com>
2024-03-26 11:46:25 -0300

    get all jobs route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,8 +2,9 @@ const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
-const getAllJobs = (req, res) => {
-  res.send('get all jobs')
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 const getJob = (req, res) => {
   res.send('get job')
