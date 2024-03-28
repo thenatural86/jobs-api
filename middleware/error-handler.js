043974d8fb7d664748f84af2eb6cf9e1c2849c4c
thenatural86 <philip.smalls86@gmail.com>
2024-03-28 10:56:15 -0300

    remove unused import

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -6,15 +5,17 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong, try again later',
   }
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`
     customError.statusCode = 400
   }
+
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   // return res.status(customError.statusCode).json({ msg: customError.msg })
 }
